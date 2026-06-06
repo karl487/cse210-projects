d23ff1242f95c9ceb9cbdf95d5cc8d9dbcbb0b3d
karl487 <ksmith35@byupathway.edu>
2026-06-06 21:01:33 +0300

    commit

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,18 @@
 using System;
-using System.Diagnostics;
-
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"The fraction is: {fraction1.GetFractionString()}");
+        Console.WriteLine($"The decimal value is: {fraction1.GetDecimalValue()}");
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine($"The fraction is: {fraction2.GetFractionString()}");
+        Console.WriteLine($"The decimal value is: {fraction2.GetDecimalValue()}");
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine($"The fraction is: {fraction3.GetFractionString()}");
+        Console.WriteLine($"The decimal value is: {fraction3.GetDecimalValue()}");
     }
 }
